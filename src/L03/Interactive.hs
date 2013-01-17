075731ff6f75579fcccf376bce234ce3daafcdfc
Tony Morris <tmorris@tmorris.net>
2013-01-17 16:54:24 +1000

    interactive exercise

--- src/L03/Interactive.hs
+++ src/L03/Interactive.hs
@@ -1,16 +1,27 @@
 module L03.Interactive where
 
+import L03.Moonad
 
+-- | An infix, flipped version of @bind@.
+(>>==) ::
+  Moonad m =>
+  m a
+  -> (a -> m b)
+  -> m b
+(>>==) =
+  flip bind
+
+-- | Runs an action until a result of that action satisfies a given predicate.
 untilM ::
-  Monad m =>
-  (a -> m Bool)
-  -> m a
+  Moonad m =>
+  (a -> m Bool) -- ^ The predicate to satisfy to stop running the action.
+  -> m a -- ^ The action to run until the predicate satisfies.
   -> m a
 untilM p a =
-  do r <- a
-     q <- p r
-     if q
-       then
-         return r
-      else
-         untilM p a
+  a >>== \r ->
+  p r >>== \q ->
+  if q
+    then
+      reeturn r
+    else
+      untilM p a
